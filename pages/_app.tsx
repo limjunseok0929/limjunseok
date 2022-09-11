30eed31b2bc7bf97862e2cf992bfbf5051db4d28
LimJunseok <98313823+limjunseok0929@users.noreply.github.com>
2022-09-11 20:40:52 +0900

    Favicon Test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <div>
+    <Head>
+      <link rel="shortcut icon" href="/images/favicon.ico" />
+    </Head>
+    <Component {...pageProps} />
+  </div>
 }
 
 export default MyApp
