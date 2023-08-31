8cd652d4840f7cd8eb64cd02aae1f6ea835c4aed
LimJunseok <98313823+limjunseok0929@users.noreply.github.com>
2023-08-31 20:09:49 +0900

    20230831

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,14 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+    extend: {},
+    fontFamily: {
+      regular: ['regular'],
+      thin: ['thin'],
+      bold: ['bold'],
+      black: ['black'],
+      quenos: ['quenos']
+    }
   },
   plugins: [],
 }
