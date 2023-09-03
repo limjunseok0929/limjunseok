d798826150b20c515a7b622fa477247460e93037
LimJunseok <98313823+limjunseok0929@users.noreply.github.com>
2023-09-04 01:27:23 +0900

    20230904

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
 import Link from 'next/link';
 import SkillBtn from './components/SkillBtn';
 import SocialBtn from './components/SocialBtn';
@@ -44,6 +43,7 @@ const SocialIconsArray = [
   ]
 ]
 
+const date = new Date()
 
 const Home: NextPage = () => {
   return (
@@ -51,10 +51,10 @@ const Home: NextPage = () => {
       <Head>
         <title>Junseok Lim</title>
       </Head>
-      {/* Main */}
-      <div className='snap-center grid content-center h-screen'>
-        <div className='flex flex-col justify-center space-y-1 absolute h-4/5 w-full'>
-          <div className='flex flex-col self-center items-center w-11/12 md:w-5/6 lg:w-4/5'>
+
+      <div className='snap-start grid h-screen'>
+        <div className='flex flex-col pt-20 space-y-1 absolute h-4/5 w-full'>
+          <div className='flex flex-col self-center w-11/12 md:w-5/6 lg:w-4/5'>
             <div className='space-y-2'>
               <h1 className='font-thin text-2xl sm:text-3xl md:text-4xl'>Junseok Lim</h1>
               <h1 className='text-3xl sm:text-4xl md:text-5xl font-bold'>A <span className='text-3xl sm:text-4xl md:text-5xl font-bold text-transparent bg-clip-text from-[#7194FF] to-[#A855F7] bg-gradient-to-r'>Student</span> Developer.</h1>
@@ -63,15 +63,15 @@ const Home: NextPage = () => {
           </div>
         </div>
       </div>
-      {/* Skills */}
-      <div className='snap-center grid content-center h-screen'>
-        <div className='flex flex-col justify-center space-y-1 absolute h-4/5 w-full'>
-          <div className='flex flex-col self-center items-center w-11/12 md:w-5/6 lg:w-4/5 space-y-5'>
+
+      <div className='snap-start grid h-screen'>
+        <div className='flex flex-col pt-20 space-y-1 absolute h-4/5 w-full'>
+          <div className='flex flex-col self-center w-11/12 md:w-5/6 lg:w-4/5 space-y-5'>
             <div>
               <h1 className='text-3xl sm:text-4xl md:text-5xl font-bold'>My <span className='text-3xl sm:text-4xl md:text-5xl text-transparent bg-clip-text from-[#7194FF] to-[#A855F7] bg-gradient-to-r font-bold'>Skills</span></h1>
               <h1 className='font-thin text-base sm:text-lg md:text-xl'>What I can use in programming</h1>
             </div>
-            <div className='flex flex-row flex-grow flex-wrap justify-center pt-4 space-x-6 sm:space-x-8 md:space-x-10 items-center text-center'>
+            <div className='flex flex-row flex-grow flex-wrap pt-4 space-x-6 sm:space-x-8 md:space-x-10 items-center text-center'>
               {
                 SkillIconsArray.map((_,i) => (
                   <div key={i}>
@@ -83,15 +83,15 @@ const Home: NextPage = () => {
           </div>
         </div>
       </div>
-      {/* Projects */}
-      <div className='snap-center grid content-center h-screen'>
-        <div className='flex flex-col justify-center space-y-1 absolute h-4/5 w-full'>
-          <div className='flex flex-col self-center items-center w-11/12 md:w-5/6 lg:w-4/5 space-y-5'>
+
+      <div className='snap-start grid h-screen'>
+        <div className='flex flex-col pt-20 space-y-1 absolute h-4/5 w-full'>
+          <div className='flex flex-col self-center w-11/12 md:w-5/6 lg:w-4/5 space-y-5'>
             <div>
               <h1 className='text-3xl sm:text-4xl md:text-5xl font-bold'>My <span className='text-3xl sm:text-4xl md:text-5xl text-transparent bg-clip-text from-[#7194FF] to-[#A855F7] bg-gradient-to-r font-bold'>Projects</span></h1>
               <h1 className='font-thin text-base sm:text-lg md:text-xl'>The projects that I made by programming</h1>
             </div>
-            <div className='flex flex-row flex-grow flex-wrap justify-center pt-4 space-x-10 items-center text-center'>
+            <div className='flex flex-row flex-grow flex-wrap pt-4 space-x-10 items-center text-center'>
               <p className='text-sm sm:text-base md:text-lg'>
                 Not any projects yet.
               </p>
@@ -99,15 +99,15 @@ const Home: NextPage = () => {
           </div>
         </div>
       </div>
-      {/* Socials */}
-      <div className='snap-center grid content-center h-screen'>
-        <div className='flex flex-col justify-center space-y-1 absolute h-4/5 w-full'>
-          <div className='flex flex-col self-center items-center w-11/12 md:w-5/6 lg:w-4/5 space-y-5'>
+
+      <div className='snap-start grid h-screen'>
+        <div className='flex flex-col pt-20 space-y-1 absolute h-4/5 w-full'>
+          <div className='flex flex-col self-center w-11/12 md:w-5/6 lg:w-4/5 space-y-5'>
             <div>
               <h1 className='text-3xl sm:text-4xl md:text-5xl font-bold'>My <span className='text-3xl sm:text-4xl md:text-5xl text-transparent bg-clip-text from-[#7194FF] to-[#A855F7] bg-gradient-to-r font-bold'>Socials</span></h1>
               <h1 className='font-thin text-base sm:text-lg md:text-xl'>You can see me on Social Network</h1>
             </div>
-            <div className='flex flex-row flex-grow flex-wrap justify-center pt-4 space-x-6 sm:space-x-8 md:space-x-10 items-center text-center'>
+            <div className='flex flex-row flex-grow flex-wrap pt-4 space-x-6 sm:space-x-8 md:space-x-10 items-center text-center'>
               {
                 SocialIconsArray.map((_,i) => (
                   <div key={i}>
@@ -119,17 +119,14 @@ const Home: NextPage = () => {
           </div>
         </div>
       </div>
-      {/* Footer */}
+
       <footer className='flex justify-center items-center snap-end text-center'>
         <div>
           <Link target='_blank' href='https://github.com/limjunseok0929/limjunseok' className='hover:underline text-xs sm:text-sm md:text-base'>
             Made With ❤️
           </Link>
           <div className='flex items-center space-x-2'>
-            <h1 className='text-xs sm:text-sm md:text-base'>&copy; Junseok Lim</h1>
-            <div className='w-4 sm:w-5 md:'>
-              <Image src='/img/profile-t.png' alt='profile' width={100} height={100} />
-            </div>
+            <h1 className='text-xs sm:text-sm md:text-base'>Copyright &copy; {date.getFullYear()}, Junseok Lim. All Rights Reserved.</h1>
           </div>
         </div>
       </footer>
